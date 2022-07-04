49680d04ab8a831d98e3215f0a178facac5f7a14
Patrick Furtak <patrick@patrickfurtak.com>
2022-07-04 14:13:52 -0500

    Update and add http requests
    Updated Angular to resolve in memory db dependency issue
    
    Created a simulated db and http call
    
    Added error handling / logging during network requests
    
    Added crud operations against mock db
    
    Added hero-search component

--- toh-client/src/app/hero-detail/hero-detail.component.ts
+++ toh-client/src/app/hero-detail/hero-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { Location } from '@angular/common';
 import { HeroService } from '../hero.service';
@@ -10,7 +10,7 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./hero-detail.component.css'],
 })
 export class HeroDetailComponent implements OnInit {
-  @Input() hero?: Hero;
+  hero: Hero | undefined;
 
   constructor(
     private route: ActivatedRoute,
@@ -23,11 +23,17 @@ export class HeroDetailComponent implements OnInit {
   }
 
   getHero(): void {
-    const id = Number(this.route.snapshot.paramMap.get('id'));
+    const id = parseInt(this.route.snapshot.paramMap.get('id')!, 10);
     this.heroService.getHero(id).subscribe((hero) => (this.hero = hero));
   }
 
   goBack(): void {
     this.location.back();
+  }
+
+  save(): void {
+    if (this.hero) {
+      this.heroService.updateHero(this.hero).subscribe(() => this.goBack());
+    }
   }
 }
